c624eed14d2c60ecd8553843976740d9b1f6c61e
Timothy Gan <timgan@Timothys-MacBook-Pro.local>
2020-03-11 17:22:04 -0500

    added knn

--- saratoga_lm.R
+++ saratoga_lm.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(mosaic)
 data(SaratogaHouses)
 
+set.seed(3)
 summary(SaratogaHouses)
 
 # Baseline model
@@ -60,9 +61,14 @@ rmse(saratoga_test$price, yhat_test3)
 
 # easy averaging over train/test splits
 library(mosaic)
+library(FNN)
 
 n_train = round(0.8*n)  # round to nearest integer
 n_test = n - n_train
+
+
+
+
 
 rmse_vals = do(100)*{
   
@@ -71,14 +77,6 @@ rmse_vals = do(100)*{
   test_cases = setdiff(1:n, train_cases)
   saratoga_train = SaratogaHouses[train_cases,]
   saratoga_test = SaratogaHouses[test_cases,]
-  
-  
-  #KNN
-  X_train = select(saratoga_train, . - fireplaces - sewer - heating)
-  y_train = select(saratoga_train, price)
-  X_test = select(saratoga_test, . - fireplaces - sewer - heating)
-  y_test = select(saratoga_test, price)
-  
   
   # Fit to the training data
   lm1 = lm(price ~ lotSize + bedrooms + bathrooms, data=saratoga_train)
@@ -92,21 +90,70 @@ rmse_vals = do(100)*{
   
 
   
-  nostuff = lm(price ~ . - fireplaces - sewer - heating, data=saratoga_train)
+  our_model = lm(price ~ . - fireplaces - sewer - heating, data=saratoga_train)
+  
+  
+  #KNN
+  Xtrain = model.matrix(~ . - (price + sewer + fireplaces + heating) - 1, data=saratoga_train)
+  Xtest = model.matrix(~ . - (price + sewer + fireplaces + heating) - 1, data=saratoga_test)
+
+  ytrain = saratoga_train$price
+  ytest = saratoga_test$price
+
+  scale_train = apply(Xtrain, 2, sd)
+  Xtilde_train = scale(Xtrain, scale = scale_train)
+  Xtilde_test = scale(Xtest, scale = scale_train)
+
+  head(Xtrain, 2)
+  head(Xtilde_train, 2) %>% round(3)
+  knn_model = knn.reg(Xtilde_train, Xtilde_test, ytrain, k=7)
+  
   # Predictions out of sample
   yhat_test1 = predict(lm1, saratoga_test)
   yhat_test2 = predict(lm2, saratoga_test)
   yhat_test3 = predict(lm3, saratoga_test)
   yhat_test4 = predict(lm_dominate, saratoga_test)
-  yhat_test5 = predict(nostuff, saratoga_test)
+  yhat_test5 = predict(our_model, saratoga_test)
 
   
   c(rmse(saratoga_test$price, yhat_test1),
     rmse(saratoga_test$price, yhat_test2),
     #rmse(saratoga_test$price, yhat_test3),
     rmse(saratoga_test$price, yhat_test4),
-    rmse(saratoga_test$price, yhat_test5))
+    rmse(saratoga_test$price, yhat_test5),
+    rmse(ytest, knn_model$pred))
 }
+
+# Xtrain = model.matrix(~ .  - (price + sewer + fireplaces + heating) - 1, data=saratoga_train)
+# Xtest = model.matrix(~ .  - (price + sewer + fireplaces + heating) - 1, data=saratoga_test)
+# 
+# ytrain = saratoga_train$price
+# ytest = saratoga_test$price
+# 
+# scale_train = apply(Xtrain, 2, sd) # calculate std dev for each column
+# Xtilde_train = scale(Xtrain, scale = scale_train)
+# Xtilde_test = scale(Xtest, scale = scale_train)
+# 
+# head(Xtrain, 2)
+# head(Xtilde_train, 2) %>% round(3)
+# 
+# library(FNN)
+# 
+# kframe <- data.frame("K" = c(), "RMEAN" =c())
+# i <- 3
+# while (i <= 250) {
+#   knn_model = knn.reg(Xtilde_train, Xtilde_test, ytrain, k=i)
+#   d = data.frame("K" = i, "RMEAN" = rmse(ytest, knn_model$pred))
+# 
+#   kframe = rbind(kframe, d)
+#   i = i + 1
+# }
+# 
+# k_vs_rmean = ggplot(data = kframe) +
+#   geom_point(mapping = aes(x = K, y = RMEAN), color='lightgrey') +
+#   theme_bw(base_size=18) + geom_path(aes(x = K, y = RMEAN), color='red')
+# 
+# k_vs_rmean
 
 rmse_vals
 colMeans(rmse_vals)
@@ -116,21 +163,4 @@ boxplot(rmse_vals)
 
 
 
-
-
-
-
-# KNN 250
-
-
-knn250 = knn.reg(train = X_train, test = X_test, y = y_train, k=250)
-ypred_knn250 = knn250$pred
-test$ypred_knn250 = ypred_knn250
-
-p_test_250 = ggplot(data = test) + 
-  geom_point(mapping = aes(x = mileage, y = price), color='lightgrey') + 
-  theme_bw(base_size=18) + geom_path(aes(x = mileage, y = ypred_knn250), color='red')
-
-p_test_250
-rmse(y_test, ypred_knn250)
 
