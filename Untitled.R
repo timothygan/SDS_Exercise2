909a37f3d8bcd5827fdf475453495791aa01d99d
Timothy Gan <timothygan@utexas.edu>
2020-04-01 23:21:31 -0500

    finished phase 1 of question 3

--- Untitled.R
+++ Untitled.R
@@ -5,7 +5,16 @@ news = read.csv("data/online_news.csv")
 rmse = function(y, ypred) {
   sqrt(mean(data.matrix((y-ypred)^2)))
 }
+
+is.nan.data.frame <- function(x)
+  do.call(cbind, lapply(x, is.nan))
+
+pcg = function(table) {
+  (table[1] + table[4])/(table[1] + table[2] + table[3] + table[4])
+}
+
 set.seed(3)
+
 news = online_news
 sum(news$shares > 1400)
 sum(news$shares <= 1400)
@@ -22,55 +31,106 @@ test_cases = setdiff(1:n, train_cases)
 news_train = news[train_cases,]
 news_test = news[test_cases,]
   
-# Fit to the training data
 
-our_model = lm(shares ~ n_tokens_title + num_imgs + num_keywords + 
-                 data_channel_is_entertainment + data_channel_is_tech +
-                  title_subjectivity + title_sentiment_polarity + avg_negative_polarity, data=news_train)
 
-our_model2 = lm(shares ~ n_tokens_title + title_subjectivity + title_sentiment_polarity + avg_negative_polarity, data=news_train)
 
-our_model3 = lm(shares ~ num_hrefs, data=news_train)
+Xtrain = model.matrix(~ num_imgs + n_tokens_title + data_channel_is_entertainment +
+                        data_channel_is_world + self_reference_avg_sharess + 
+                        global_rate_positive_words + global_rate_negative_words +
+                        avg_negative_polarity + data_channel_is_socmed, data=news_train)
+Xtest = model.matrix(~ num_imgs + n_tokens_title + data_channel_is_entertainment +
+                       data_channel_is_world + self_reference_avg_sharess + 
+                       global_rate_positive_words + global_rate_negative_words +
+                       avg_negative_polarity + data_channel_is_socmed, data=news_test)
 
-our_model4 = lm(shares ~ n_tokens_title + data_channel_is_entertainment +
-                  data_channel_is_world + self_reference_max_shares + 
-                  global_rate_positive_words + global_rate_negative_words +
-                  min_positive_polarity + max_positive_polarity + 
-                  min_negative_polarity + max_negative_polarity +
-                  title_subjectivity + title_sentiment_polarity, data=news_train)
+ytrain = news_train$shares
+ytest = news_test$shares
 
-our_model5 = lm(shares ~ n_tokens_title + data_channel_is_entertainment +
-                  data_channel_is_world + self_reference_max_shares + 
-                  global_rate_positive_words + global_rate_negative_words +
-                  avg_negative_polarity + 
-                  min_negative_polarity + max_negative_polarity +
-                  title_subjectivity + title_sentiment_polarity, data=news_train)
-# Predictions out of sample
-coef(our_model4)
-yhat_test = predict(our_model, news_test)
-yhat_test2 = predict(our_model2, news_test)
-yhat_test3 = predict(our_model3, news_test)
-yhat_test4 = predict(our_model4, news_test)
-yhat_test5 = predict(our_model5, news_test)
+scale_train = apply(Xtrain, 2, sd)
+Xtilde_train = scale(Xtrain, scale = scale_train)
+Xtilde_test = scale(Xtest, scale = scale_train)
 
-yhat_test_viral = ifelse(yhat_test > 1400, 1, 0) 
+Xtilde_train[is.nan(Xtilde_train)] <- 0
+Xtilde_test[is.nan(Xtilde_test)] <- 0
+head(Xtrain, 2)
+head(Xtilde_train, 2) %>% round(3)
+knn_model = knn.reg(Xtilde_train, Xtilde_test, ytrain, k=5)
+
+
+
+
 news_test_viral = ifelse(news_test$shares > 1400, 1, 0) 
 
-yhat_test_viral2 = ifelse(yhat_test2 > 1400, 1, 0) 
-yhat_test_viral3 = ifelse(yhat_test3 > 1400, 1, 0) 
-yhat_test_viral4 = ifelse(yhat_test4 > 1400, 1, 0) 
-yhat_test_viral5 = ifelse(yhat_test5 > 1400, 1, 0) 
+knn_test_viral = ifelse(knn_model$pred > 1400, 1, 0) 
 
-confusion_out = table(y = news_test_viral, yhat = yhat_test_viral)
+confusion_out = table(y = news_test_viral, o = knn_test_viral)
 confusion_out
-confusion_out2 = table(y = news_test_viral, yhat = yhat_test_viral2)
-confusion_out2
-confusion_out3 = table(y = news_test_viral, yhat = yhat_test_viral3)
-confusion_out3
-confusion_out4 = table(y = news_test_viral, yhat = yhat_test_viral4)
-confusion_out4
-confusion_out5 = table(y = news_test_viral, yhat = yhat_test_viral5)
-confusion_out5
+pec = pcg(confusion_out)
+print("percentage correct:")
+pec
+print("true positive rate:")
+confusion_out[4]/(confusion_out[2] +confusion_out[4])
+print("false positive rate:") 
+confusion_out[2]/(confusion_out[2] +confusion_out[4])
+
+i <- 3
+while(i <= 50){
+  train_cases = sample.int(n, n_train, replace=FALSE)
+  test_cases = setdiff(1:n, train_cases)
+  news_train = news[train_cases,]
+  news_test = news[test_cases,]
+  
+  
+  
+  
+  Xtrain = model.matrix(~ num_imgs + n_tokens_title + data_channel_is_entertainment +
+                          data_channel_is_world + self_reference_avg_sharess + 
+                          global_rate_positive_words + global_rate_negative_words +
+                          avg_negative_polarity + data_channel_is_socmed, data=news_train)
+  Xtest = model.matrix(~ num_imgs + n_tokens_title + data_channel_is_entertainment +
+                         data_channel_is_world + self_reference_avg_sharess + 
+                         global_rate_positive_words + global_rate_negative_words +
+                         avg_negative_polarity + data_channel_is_socmed, data=news_test)
+  
+  ytrain = news_train$shares
+  ytest = news_test$shares
+  
+  scale_train = apply(Xtrain, 2, sd)
+  Xtilde_train = scale(Xtrain, scale = scale_train)
+  Xtilde_test = scale(Xtest, scale = scale_train)
+  Xtilde_train[is.nan(Xtilde_train)] <- 0
+  Xtilde_test[is.nan(Xtilde_test)] <- 0
+  head(Xtrain, 2)
+  head(Xtilde_train, 2) %>% round(3)
+  knn_model = knn.reg(Xtilde_train, Xtilde_test, ytrain, k=5)
+  
+  
+  
+  
+  news_test_viral = ifelse(news_test$shares > 1400, 1, 0) 
+  
+  knn_test_viral = ifelse(knn_model$pred > 1400, 1, 0) 
+  
+  new_confusion_out = table(y = news_test_viral, o = knn_test_viral)
+  
+  confusion_out[1] = (confusion_out[1]+new_confusion_out[1])/2
+  confusion_out[2] = (confusion_out[2]+new_confusion_out[2])/2
+  confusion_out[3] = (confusion_out[3]+new_confusion_out[3])/2
+  confusion_out[4] = (confusion_out[4]+new_confusion_out[4])/2
+  
+  pec = (pec + pcg(new_confusion_out))/2
+  i = i + 1
+}
+
+confusion_out
+pec
+print("true positive rate:")
+confusion_out[4]/(confusion_out[2] +confusion_out[4])
+print("false positive rate:") 
+confusion_out[2]/(confusion_out[2] +confusion_out[4])
+
+
+
 
 
 
